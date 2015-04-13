db3fe3d2562ccc8eec87efdf7fe220c89e3eeb41
Philip Cali <philip.cali@bronto.com>
2015-04-12 22:21:10 -0400

    Moar codez.

--- src/Bronto/Transfer/Response.php
+++ src/Bronto/Transfer/Response.php
@@ -10,6 +10,8 @@ namespace Bronto\Transfer;
 interface Response
 {
     const OK = 200;
+    const CREATED = 201;
+    const NO_CONTENT = 204;
     const BAD_REQUEST = 400;
     const UNAUTHORIZED = 401;
     const FORBIDDEN = 403;
