149dcedcb3c9b2a7bad5a2e9eae55206ab4cea8c
Philip Cali <philip.cali@bronto.com>
2015-04-12 22:01:53 -0400

    Simple pluralize.

--- src/Bronto/Utils.php
+++ src/Bronto/Utils.php
@@ -2,6 +2,12 @@
 
 namespace Bronto;
 
+/**
+ * A simple utility class containing simple string
+ * manipulations.
+ *
+ * @author Philip Cali <philip.cali@bronto.com>
+ */
 class Utils
 {
     /**
@@ -15,5 +21,21 @@ class Utils
         $pattern = '/([a-z0-9])([A-Z])/';
         $underscores = preg_replace($pattern, "$1_$2", $name);
         return strtolower($underscores);
+    }
+
+    /**
+     * Simple pluralization for common names
+     *
+     * @param string $name
+     * @return string
+     */
+    public static function pluarize($name)
+    {
+       if (preg_match('/y$/', $name)) {
+            $name = preg_replace('/y$/', 'ies', $name);
+        } else {
+            $name .= 's';
+        }
+        return $name;
     }
 }
